b4eec73f8271b01ab56e2c2a30067e72b4ad3827
raksha <rakshams06@gmail.com>
2026-09-20 10:33:39 +0530

    Declare DOM element variables

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// DOM Elements
+const itemNameInput = document.getElementById('itemName');
+const itemPriceInput = document.getElementById('itemPrice');
+const addItemBtn = document.getElementById('addItemBtn');
+const groceryList = document.getElementById('groceryList');
+const totalExpenseDisplay = document.getElementById('totalExpense');
