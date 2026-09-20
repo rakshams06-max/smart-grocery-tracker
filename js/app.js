73b8d35d89ed982f15c6839262fd1451fe2b58f8
raksha <rakshams06@gmail.com>
2026-09-20 10:34:47 +0530

    Initialize state array for items and total expense variable

--- js/app.js
+++ js/app.js
@@ -3,4 +3,6 @@ const itemNameInput = document.getElementById('itemName');
 const itemPriceInput = document.getElementById('itemPrice');
 const addItemBtn = document.getElementById('addItemBtn');
 const groceryList = document.getElementById('groceryList');
-const totalExpenseDisplay = document.getElementById('totalExpense');
+const totalExpenseDisplay = document.getElementById('totalExpense');// State Data
+let groceryItems = [];
+let totalExpense = 0;
