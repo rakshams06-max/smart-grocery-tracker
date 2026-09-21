8585871031f642b4d9f41481db9b1b472026fbce
mukunda <mukundap1266@gmail.com>
2026-09-21 19:47:23 +0530

    Add HTML Delete button element to item template

--- js/app.js
+++ js/app.js
@@ -40,6 +40,7 @@ function addItem() {
         li.innerHTML = `
             <input type="checkbox" ${item.bought ? 'checked' : ''} onchange="toggleBoughtStatus(${item.id})">
             <span>${item.name} - ₹${item.price.toFixed(2)}</span>
+            <button class="delete-btn">Delete</button>
         `;
         groceryList.appendChild(li);
     });
