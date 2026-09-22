e1c53928781586d09c5025da70e9f1cdbba319b7
Rohan Sagar <rohan.s2@s.amity.edu>
2026-09-22 10:36:11 +0530

    Connect budget check to expense update

--- js/app.js
+++ js/app.js
@@ -68,6 +68,7 @@ function addItem() {
     
 }function updateTotal() {
     totalExpenseDisplay.textContent = totalExpense.toFixed(2);
+    checkBudget();
 }// Event Listeners
 addItemBtn.addEventListener('click', addItem);
 function toggleBoughtStatus(id) {
